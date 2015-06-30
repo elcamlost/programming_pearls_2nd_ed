1d555c74a7879e5de41d1684f38e7e4d46d020a3
Ilya "elcamlost" Rassadin <elcamlost@gmail.com>
2015-06-30 17:19:29 +0300

    add solution for Problem_01

--- 1_Cracking_the_Oyster/Problem_01/sort_numbers.pl
+++ 1_Cracking_the_Oyster/Problem_01/sort_numbers.pl
@@ -4,28 +4,51 @@ use warnings FATAL => 'all';
 use autodie;
 use v5.18;
 
-use FILE_CONFIGURATION;
+use constant MAXIMUM_VECTOR_SIZE => 1000 * 16;
 
-open(my $input_file, '<', "./input_file.txt");
+open(my $input_file, '<', "../input_file.txt");
 
 #initialize vector
 my $sorting_numbers = '';
-vec($sorting_numbers, FILE_CONFIGURATION::MAXIMUM_NUMBERS_COUNT - 1, 1) = 0;
+vec($sorting_numbers, MAXIMUM_VECTOR_SIZE - 1, 1) = 0;
 
-#"sort" numbers
+#"sort" numbers, number must be lower or equal to MAXIMUM_VECTOR_SIZE
 while(<$input_file>) {
   # remove newlines
   chomp;
-  vec($sorting_numbers, $_, 1) = 1;
+  if($_ <= MAXIMUM_VECTOR_SIZE) {
+    vec($sorting_numbers, $_, 1) = 1;
+  }
 }
 
-close($input_file);
-open(my $output_file, '>', "./output_file.txt");
+open(my $output_file, '>', "../output_file.txt");
 
 #print sorted numbers
-for(my $bitVectorIndex = 1; $bitVectorIndex <= FILE_CONFIGURATION::MAXIMUM_NUMBERS_COUNT; $bitVectorIndex ++) {
-  if(vec($sorting_numbers, $bitVectorIndex, 1) == 1) {
-    say $output_file $bitVectorIndex;
+for(my $sorting_vector_index = 1; $sorting_vector_index <= MAXIMUM_VECTOR_SIZE; $sorting_vector_index ++) {
+  if(vec($sorting_numbers, $sorting_vector_index, 1) == 1) {
+    say $output_file $sorting_vector_index;
+  }
+}
+
+#reinitialize
+$sorting_numbers = '';
+vec($sorting_numbers, MAXIMUM_VECTOR_SIZE - 1, 1) = 0;
+seek($input_file, 0, 0);
+
+#"sort" numbers again, now number must be bigger than MAXIMUM_VECTOR_SIZE
+while(<$input_file>) {
+  # remove newlines
+  chomp;
+  if($_ > MAXIMUM_VECTOR_SIZE) {
+    vec($sorting_numbers, $_ - MAXIMUM_VECTOR_SIZE, 1) = 1;
+  }
+}
+close $input_file;
+
+#print sorted numbers
+for(my $sorting_vector_index = 1; $sorting_vector_index <= MAXIMUM_VECTOR_SIZE; $sorting_vector_index ++) {
+  if(vec($sorting_numbers, $sorting_vector_index, 1) == 1) {
+    say $output_file (MAXIMUM_VECTOR_SIZE + $sorting_vector_index);
   }
 }
 
